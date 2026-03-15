a9eeeba0b0aba564dbffc6dd567ff1455b0ba428
Ethan Kowatsch <ethank3299@gmail.com>
2026-03-15 15:49:05 -0400

    Completed folder structure practice problem

---
+++ testing_code/folder_structure/app.js
@@ -0,0 +1,5 @@
+import { add, subtract } from "./utils/math.js";
+
+console.log(`2 + 3 = ${add(2, 3)}`);
+
+console.log(`5 - 1 = ${subtract(5, 1)}`);
---
+++ testing_code/folder_structure/utils/math.js
@@ -0,0 +1,21 @@
+/**
+ * Function to add two numbers
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ * @returns The sum of the x and y variable.
+ */
+export function add(x, y) {
+    return x + y;
+}
+
+/**
+ * Function to subtract two numbers
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ * @returns Returns the difference between x and y.
+ */
+export function subtract(x, y) {
+    return x - y;
+}
