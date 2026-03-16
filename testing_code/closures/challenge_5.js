9c36b29469177fb909553c95e1334b325025b945
Ethan Kowatsch <ethank3299@gmail.com>
2026-03-16 14:52:38 -0400

    Completed challenges 5

---
+++ testing_code/closures/challenge_5.js
@@ -0,0 +1,24 @@
+function createLimitedUseFunction(fn, limit) {
+    let counterToLimit = 0;
+
+    return function(...args) {
+        counterToLimit++;
+
+        if(counterToLimit <= limit) {
+            return fn(...args);
+        }
+
+        console.log(`Reached limit on function use. Limit is ${limit}`);
+    }
+}
+
+function greet(userName) {
+    return console.log(`Hello ${userName}`);
+}
+
+const limitedGreet = createLimitedUseFunction(greet, 3);
+
+limitedGreet("Ethan");
+limitedGreet("Alex");
+limitedGreet("Bob");
+limitedGreet("John");
