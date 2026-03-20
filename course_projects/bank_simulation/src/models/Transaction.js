0cf92df4b6c736f8fb2d7585ed034de3a014e1b0
Ethan Kowatsch <ethank3299@gmail.com>
2026-03-20 14:22:37 -0400

    Setup structure of the transaction class

---
+++ course_projects/bank_simulation/src/models/Transaction.js
@@ -0,0 +1,56 @@
+import { getCurrentDateAndTime, checkIfAccountExists } from "../utils/helper.js";
+import { accountsArray } from "./Account.js";
+
+export let transactionArray = [];
+
+/**
+ * Represents a bank transaction.
+ * Responsible for creating transaction data.
+ */
+export class Transaction {
+    /**
+     * @param {string} transactionType - Type of transaction (deposit, withdrawal, transfer)
+     * @param {number} transactionAmount - Amount involved in transaction
+     * @param {number} accountNumberAffected - Account linked to transaction
+     */
+    constructor(transactionType, transactionAmount, accountNumberAffected) {
+        this.transactionType = transactionType;
+        this.transactionAmount = transactionAmount;
+        this.accountNumberAffected = accountNumberAffected;
+        this.timeStamp = getCurrentDateAndTime();
+    }
+
+    set transactionType(transactionTypeValue) {
+        if(!transactionTypeValue) return //TODO: Add custom error;
+
+        this._transactionType = transactionTypeValue;
+    }
+
+    /**
+     * 
+     */
+    set transactionAmount(transactionAmountValue) {
+        if(transactionAmountValue <= 0) return //TODO: Add custom error;
+
+        this._transactionAmount = transactionAmountValue;
+    }
+
+    set accountNumberAffected(accountNumberAffectedValue) {
+        const accountExists = checkIfAccountExists(accountNumberAffectedValue);
+
+        if(!accountExists) return //TODO: Add custom error;
+
+        this._accountNumberAffected = accountNumberAffectedValue;
+    }
+
+    get transactionType() { return this._transactionType };
+    get transactionAmount() { return this._transactionAmount };
+    get accountNumberAffected() { return this._accountNumberAffected };
+
+    /**
+     * Logs transaction to transactions.json
+     */
+    logTransaction() {
+
+    }
+}
