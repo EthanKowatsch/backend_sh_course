f8c576e973c20836df32a1f8efe0b7ceaa7dccb2
Ethan Kowatsch <ethank3299@gmail.com>
2026-03-21 11:34:46 -0400

    Finished JSON handler file

--- course_projects/bank_simulation/src/utils/jsonHandler.js
+++ course_projects/bank_simulation/src/utils/jsonHandler.js
@@ -1,23 +1,32 @@
 import fs from "fs";
 import { accountsArray } from "../models/Account.js";
 import { transactionArray } from "../models/Transaction.js";
+import { JSONLoadingError, JSONUpdatingError } from "../models/errors.js";
 
 /**
  * Function to load the accountsArray with the different accounts
  */
 export function loadAccounts() {
     try {
+        if (!fs.existsSync("data/accounts.json")) {
+            accountsArray.length = 0;
+            return;
+        }
+
         const jsonAccounts = fs.readFileSync("data/accounts.json", "utf8");
 
-        if (!jsonAccounts.trim()) return // TODO: Add custom error if now accounts exist;
+        if (!jsonAccounts || !jsonAccounts.trim()) {
+            accountsArray.length = 0;
+            return;
+        }
 
         const tempAccountsObj = JSON.parse(jsonAccounts);
 
         accountsArray.length = 0;
-        accountsArray.push(...tempAccountsObj);
+        accountsArray.push(...tempAccountsObj.map(a => new Account(a.userName, a.accountType, a.balance, a.accountNumber)));
     }
     catch(err) {
-        // TODO: Custom error when the json doesn't work
+        throw new JSONLoadingError("Failed to load accounts JSON.", err);
     }
 }
 
@@ -29,7 +38,7 @@ export function updateAccounts() {
         fs.writeFileSync("data/accounts.json", JSON.stringify(accountsArray, null, 2), "utf8");
     }
     catch(err) {
-        // TODO: Custom error when the json doesn't work
+        throw new JSONUpdatingError("Accounts JSON failed to update.", err);
     }
 }
 
@@ -38,7 +47,17 @@ export function updateAccounts() {
  */
 export function loadTransactions() {
     try {
+        if (!fs.existsSync("data/transactions.json")) {
+            transactionArray.length = 0;
+            return;
+        }
+
         const jsonTransactions = fs.readFileSync("data/transactions.json", "utf8");
+
+        if (!jsonTransactions || !jsonTransactions.trim()) {
+            transactionArray.length = 0;
+            return;
+        }
 
         const tempTransactionObj = JSON.parse(jsonTransactions);
 
@@ -46,7 +65,7 @@ export function loadTransactions() {
         transactionArray.push(...tempTransactionObj);
     }
     catch(err) {
-        // TODO: Custom error when the json doesn't work
+        throw new JSONLoadingError("Failed to load transactions JSON.", err);
     }
 }
 
@@ -58,6 +77,6 @@ export function updateTransactions() {
         fs.writeFileSync("data/transactions.json", JSON.stringify(transactionArray, null, 2), "utf8");
     }
     catch(err) {
-        // TODO: Custom error when the json doesn't work
+        throw new JSONUpdatingError("Transactions JSON failed to update.", err);
     }
 }
