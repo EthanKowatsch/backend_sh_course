a1ef40e556e34b15da5fc98277c3aa2a10d39181
Ethan Kowatsch <ethank3299@gmail.com>
2026-04-16 14:58:07 -0400

    Completed exercise 3

---
+++ testing_code/practice/e_3.js
@@ -0,0 +1,10 @@
+const player = {
+    name: "Ethan",
+    goals: 2,
+    assists: 1,
+    team: "Listowel Cyclones",
+};
+
+console.log(`${player.name} - Team: ${player.team} | Goals: ${player.goals} | Assists: ${player.assists} | Total Points: ${player.goals + player.assists}`);
+
+console.log(`GOAL Scored: Goal by ${player.name} for the ${player.team}, his ${player.goals += 1} goal of the year!`);
